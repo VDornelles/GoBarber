38b902a615486ab6b14bb3779acff5409751338f
Vinicius Dornelles de Sousa <vinicius_sousa@hst.br>
2020-06-27 20:23:18 -0300

    Appoitments controller

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,37 @@
+import * as Yup from 'yup';
+import Appointment from '../models/Appointments';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+    const { provider_id, date } = req.body;
+
+    // Check if given provider_id belongs to a provider
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401) // Unauthorized access status
+        .json({ error: 'Given provider_id does not belong to a provider' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
