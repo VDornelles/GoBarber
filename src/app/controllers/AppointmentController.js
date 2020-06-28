a5fb986b935658e48879e17b1e6c2ac34c5791cd
Vinicius Dornelles de Sousa <vinicius_sousa@hst.br>
2020-06-27 23:22:08 -0300

    Appointment's date validation

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { isBefore, startOfHour, parseISO } from 'date-fns';
 import Appointment from '../models/Appointments';
 import User from '../models/User';
 
@@ -24,11 +25,29 @@ class AppointmentController {
         .status(401) // Unauthorized access status
         .json({ error: 'Given provider_id does not belong to a provider' });
     }
+    // Check if date selected is valid
+    const hourStart = startOfHour(parseISO(date));
 
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not available' });
+    }
+
+    // Check if date selected is available
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Date not available' });
+    }
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
     return res.json(appointment);
   }
